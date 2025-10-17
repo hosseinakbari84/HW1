cb397ef4e9e19fe3f70b7916f50ed4146451264d
Hossein Akbari <hossein8489@gmail.com>
2025-10-17 13:33:57 +0330

    added a vector of orderIDs attribute

--- Student.h
+++ Student.h
@@ -7,11 +7,12 @@ class Student
 public:
     std::string name;
     int studentID;
-    std::vector<Order*> orders;
+    std::vector<int> ordersID;
     Student* nextStudent = nullptr;
     std::string getName() const;
     Student(std::string n, int id);
     int getID() const;
     void setName(const std::string &n);
     void setID(int id);
+    void printStudentOrders() const;
 };
